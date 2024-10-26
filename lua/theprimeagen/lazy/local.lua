9aef72f97342d1ab9154a9bdb1221cde51b00009
chamuditha pasindu <chamudithapasindu54@gmail.com>
2024-10-27 01:43:41 +0530

    Error of the plugin Hrapoon configuration was fixed

--- lua/theprimeagen/lazy/local.lua
+++ lua/theprimeagen/lazy/local.lua
@@ -1,45 +1,39 @@
 
 local local_plugins = {
     {
-        "harpoon",
-        --[[dir = "~/personal/harpoon",]]
         "ThePrimeagen/harpoon",
         config = function()
             local harpoon = require("harpoon")
+            local mark = require("harpoon.mark")
+            local ui = require("harpoon.ui")
 
-            harpoon:setup()
+            harpoon.setup()
 
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+            vim.keymap.set("n", "<leader>a", mark.add_file) -- Add current file to harpoon
+            vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu) -- Open harpoon menu
 
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-            vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
-            vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
-            vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
+            -- Navigate to specific harpoon files
+            vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+            vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+            vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+            vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
         end
     },
     {
-        "vim-apm", --[[dir = "~/personal/vim-apm",]]
         "ThePrimeagen/vim-apm",
         config = function()
-            --[[
-            local apm = require("vim-apm")
-
-            apm:setup({})
-            vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
-            --]]
+            -- Uncomment and configure if you want to use vim-apm
+            -- local apm = require("vim-apm")
+            -- apm.setup({})
+            -- vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
         end
     },
     {
-        "vim-with-me", --[[dir = "~/personal/vim-with-me",]]
         "ThePrimeagen/vim-with-me",
-        config = function() end
+        config = function()
+            -- Additional configurations for vim-with-me can go here
+        end
     },
 }
 
 return local_plugins
-
