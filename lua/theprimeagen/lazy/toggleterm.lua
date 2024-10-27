8689498b1567bacdf8b320c33c854de9466ee9f9
chamuditha pasindu <chamudithapasindu54@gmail.com>
2024-10-27 17:50:46 +0530

    Error of the plugin Hrapoon configuration was fixed

--- lua/theprimeagen/lazy/toggleterm.lua
+++ lua/theprimeagen/lazy/toggleterm.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require("toggleterm").setup{
         size = 10,
-        open_mapping = [[<Tab>]],      -- Keybinding to toggle terminal
+        open_mapping = [[<leader>`]],      -- Keybinding to toggle terminal
         shade_terminals = true,
         direction = "horizontal",      -- Split direction (horizontal, vertical, or float)
         close_on_exit = true,
